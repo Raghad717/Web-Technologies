721db1ed516202ae95f884f8d9f76d9dc4607b85
Raghad717 <r4gh4dalmalki@gmail.com>
2026-04-24 19:20:33 +0300

    Add initial HTML structure for index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Home</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<nav>
+    <a href="index.php">Home</a>
+    <a href="register.php">Register</a>
+    <a href="appointments.php">Appointments</a>
+</nav>
+
+<div class="container">
+    <div class="card home">
+        <h1>Clinic System</h1>
+        <p>Manage your appointments easily</p>
+    </div>
+</div>
+
+</body>
+</html>
